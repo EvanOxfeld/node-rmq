30d690dcaf46280159b9657bd5c0640cfc26b81d
Christopher Gillis <christopher.gillis@vbrick.com>
2014-01-31 10:09:30 -0500

    Added config parameter to optionally configure redis instance - still isn't at the entry point for the lib, need to add that
    modified subscriptions to return a subscription id to use when unsubscribing

--- rmq.js
+++ rmq.js
@@ -2,12 +2,50 @@
 
 var publisher = require('./lib/publisher');
 
+var uuid = require('node-uuid');
+
+/**
+ * Publish a message on the message bus
+ * @param channel
+ * @param message
+ */
 exports.publish = function(channel, message){
 	publisher.publish(channel, message);
 };
 
 var subscriptions = require('./lib/subscriptions');
 
+/**
+ * @param channel - channel to listen for
+ * @param callback - function executed when channel message received
+ * @returns subscription id - this is required to unsubscribe to a channel
+ */
 exports.subscribe = function(channel, callback){
-	subscriptions.add(channel, callback);
+	var subscription = {
+		callback: callback,
+		id: generateSubscriptionId()
+	};
+	subscriptions.subscribe(channel, subscription);
+
+	return subscription.id;
 };
+
+/**
+ *
+ * @param channel - the channel to stop listening to
+ * @param subscriptionId - the subscriptionId returned by the subscribe function
+ */
+exports.unsubscribe = function(channel, subscriptionId){
+	if(!channel){
+		// TODO handle error
+	}
+	if(!subscriptionId){
+		// TODO handle error
+	}
+
+	subscriptions.unsubscribe(channel, subscriptionId);
+};
+
+function generateSubscriptionId(){
+	return uuid.v4();
+}
